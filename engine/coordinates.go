d80ceb8b9e6310f578b14d46c79155145ad0abab
Michael Henderson <mdhender@mdhender.com>
2023-03-03 13:33:56 -0700

    renamed Coordinates receiver

--- engine/coordinates.go
+++ engine/coordinates.go
@@ -14,10 +14,6 @@ import (
 //
 // EAST and WEST are on the X axis.
 // EAST is a positive X value, WEST is negative.
-//
-// Bearing is the angle between two points measured clockwise from NORTH.
-//
-// Relative bearing is measured clockwise from the ship's centerline.
 
 // Coordinates are x and y (internally).
 // At some point, we'll output them as longitude and latitude.
@@ -27,18 +23,18 @@ type Coordinates struct {
 }
 
 // String implements the Stringer interface {
-func (a Coordinates) String() string {
-	return fmt.Sprintf("(%d %d)", int(math.Floor(a.X)), int(math.Floor(a.Y)))
+func (c Coordinates) String() string {
+	return fmt.Sprintf("(%d %d)", int(math.Floor(c.X)), int(math.Floor(c.Y)))
 }
 
 // MarshalJSON implements the json.Marshaler interface.
-func (a Coordinates) MarshalJSON() ([]byte, error) {
-	return []byte(fmt.Sprintf("(%g %g)", a.X, a.Y)), nil
+func (c Coordinates) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf("(%g %g)", c.X, c.Y)), nil
 }
 
 // UnmarshalJSON implements the json.Unmarshaler interface.
-func (a *Coordinates) UnmarshalJSON(b []byte) error {
-	if n, err := fmt.Fscanf(bytes.NewReader(b), "(%g %g)", &a.X, &a.Y); n != 2 || err != nil {
+func (c *Coordinates) UnmarshalJSON(b []byte) error {
+	if n, err := fmt.Fscanf(bytes.NewReader(b), "(%g %g)", &c.X, &c.Y); n != 2 || err != nil {
 		return fmt.Errorf("invalid coordinates")
 	}
 	return nil
